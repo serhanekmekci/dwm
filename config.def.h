c0e2e3c5a97e1b615477d293489db696f5b6e8be
Serhan Ekmekçi <serhanekmekci@protonmail.com>
2020-10-23 21:11:05 +0300

    config

--- config.def.h
+++ config.def.h
@@ -4,7 +4,7 @@
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
-static const int topbar             = 0;        /* 0 means bottom bar */
+static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { 
 
 	"Fira Code:size=10:antialias=true:autohint=true", 
