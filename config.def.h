368e554b8bfa300a0f6c1885482dcf52bcefbe4d
Serhan Ekmekçi <serhanekmekci@protonmail.com>
2020-10-24 00:02:00 +0300

    config

--- config.def.h
+++ config.def.h
@@ -12,7 +12,7 @@ static const char *fonts[]          = {
 	"Font Awesome 5 Brands:size=13:antialias=true:autohint=true"
 };
 
-static const char dmenufont[]       = "Fira Code:size=10:antialias=true:autohint=true";
+static const char dmenufont[]       = "Fira Code:size=12:antialias=true:autohint=true";
 static const char col_gray1[]       = "#000000";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
@@ -63,7 +63,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, "-h", dmenu_lh, topbar ? NULL : "-b", NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, "-h", dmenu_lh, "-c", "-l 10", topbar ? NULL : "-b", NULL };
 static const char *termcmd[]  = { "st", NULL };
 
 static Key keys[] = {
