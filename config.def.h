ade635be9307298cf2ab32c08413b263a44d4780
Serhan Ekmekçi <serhanekmekci@protonmail.com>
2020-11-08 12:46:29 +0300

    font to Iosvka

--- config.def.h
+++ config.def.h
@@ -7,7 +7,7 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = {
 
-	"Input:size=10:antialias=true:autohint=true:style=Regular",
+	"Iosevka:size=10:antialias=true:autohint=true:style=Regular",
 	"Font Awesome 5 Free Solid:size=13:antialias=true:autohint=true",
 	"Font Awesome 5 Brands:size=13:antialias=true:autohint=true"
 };
