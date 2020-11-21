10ee53dcce63aac72fd479f042c26cedd6a56916
Serhan Ekmekçi <serhan@ekmekci.me>
2020-11-21 16:34:39 +0300

    scratchpads fixed

--- config.def.h
+++ config.def.h
@@ -60,9 +60,12 @@ static const Rule rules[] = {
 	/* class     instance  title           tags mask  isfloating  isterminal  noswallow  monitor */
 	{ "Gimp",    NULL,     NULL,           0,         1,          0,           0,        -1 },
 	{ "Firefox", NULL,     NULL,           1 << 8,    0,          0,          -1,        -1 },
-//	{ "St",      NULL,     NULL,           0,         0,          1,           0,        -1 },
-//	{ "tabbed",  NULL,     NULL,           0,         0,          1,           0,        -1 },
-//	{ NULL,      NULL,     "Event Tester", 0,         0,          0,           1,        -1 }, /* xev */
+	{ "St",      NULL,     NULL,           0,         0,          1,           0,        -1 },
+	{ "tabbed",  NULL,     NULL,           0,         0,          1,           0,        -1 },
+	{ NULL,      NULL,     "Event Tester", 0,         0,          0,           1,        -1 }, /* xev */
+	{ NULL,		  "sp1",		NULL,		SPTAG(0),		1,			 -1 },
+	{ NULL,		  "sp2",		NULL,		SPTAG(1),		1,			 -1 },
+
 };
 
 /* layout(s) */
