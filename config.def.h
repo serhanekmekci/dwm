a084a257ce272f27733241f322513a5a44f153c6
Serhan Ekmekçi <serhan@ekmekci.me>
2020-12-11 03:33:57 +0300

    deleted unused gaps controls and gaps values aligned.

--- config.def.h
+++ config.def.h
@@ -3,10 +3,12 @@
 /* appearance */
 static unsigned int borderpx  = 3;        /* border pixel of windows */
 static unsigned int snap      = 20;       /* snap pixel */
-static const unsigned int gappih    = 10;       /* horiz inner gap between windows */
-static const unsigned int gappiv    = 10;       /* vert inner gap between windows */
-static const unsigned int gappoh    = 10;       /* horiz outer gap between windows and screen edge */
-static const unsigned int gappov    = 10;       /* vert outer gap between windows and screen edge */
+static const int vertpad            = 20;       /* vertical padding of bar */
+static const int sidepad            = 25;       /* horizontal padding of bar */
+static const unsigned int gappih    = 20;       /* horiz inner gap between windows */
+static const unsigned int gappiv    = 20;       /* vert inner gap between windows */
+static const unsigned int gappoh    = 25+3;       /* horiz outer gap between windows and screen edge */
+static const unsigned int gappov    = 20;       /* vert outer gap between windows and screen edge */
 static const int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static int showbar            = 1;        /* 0 means no bar */
 static int topbar             = 1;        /* 0 means bottom bar */
@@ -14,8 +16,6 @@ static const int user_bh            = 30;        /* 0 means that dwm will calcul
 static const unsigned int baralpha = 0x64;
 static const unsigned int borderalpha = 0x64;
 static const unsigned int fgalpha = 0x64;
-static const int vertpad            = 20;       /* vertical padding of bar */
-static const int sidepad            = 25;       /* horizontal padding of bar */
 static char *fonts[]          = {
 	"Font Awesome 5 Free Solid:size=14:antialias=true:autohint=true",
 	"Font Awesome 5 Brands:size=14:antialias=true:autohint=true",
@@ -133,22 +133,22 @@ static Key keys[] = {
 	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
 	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
 	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
-	{ MODKEY|Mod4Mask,              XK_h,      incrgaps,       {.i = +1 } },
-	{ MODKEY|Mod4Mask,              XK_l,      incrgaps,       {.i = -1 } },
-	{ MODKEY|Mod4Mask|ShiftMask,    XK_h,      incrogaps,      {.i = +1 } },
-	{ MODKEY|Mod4Mask|ShiftMask,    XK_l,      incrogaps,      {.i = -1 } },
-	{ MODKEY|Mod4Mask|ControlMask,  XK_h,      incrigaps,      {.i = +1 } },
-	{ MODKEY|Mod4Mask|ControlMask,  XK_l,      incrigaps,      {.i = -1 } },
-	{ MODKEY|Mod4Mask,              XK_0,      togglegaps,     {0} },
-	{ MODKEY|Mod4Mask|ShiftMask,    XK_0,      defaultgaps,    {0} },
-	{ MODKEY,                       XK_y,      incrihgaps,     {.i = +1 } },
-	{ MODKEY,                       XK_o,      incrihgaps,     {.i = -1 } },
-	{ MODKEY|ControlMask,           XK_y,      incrivgaps,     {.i = +1 } },
-	{ MODKEY|ControlMask,           XK_o,      incrivgaps,     {.i = -1 } },
-	{ MODKEY|Mod4Mask,              XK_y,      incrohgaps,     {.i = +1 } },
-	{ MODKEY|Mod4Mask,              XK_o,      incrohgaps,     {.i = -1 } },
-	{ MODKEY|ShiftMask,             XK_y,      incrovgaps,     {.i = +1 } },
-	{ MODKEY|ShiftMask,             XK_o,      incrovgaps,     {.i = -1 } },
+//	{ MODKEY|Mod4Mask,              XK_h,      incrgaps,       {.i = +1 } },
+//	{ MODKEY|Mod4Mask,              XK_l,      incrgaps,       {.i = -1 } },
+//	{ MODKEY|Mod4Mask|ShiftMask,    XK_h,      incrogaps,      {.i = +1 } },
+//	{ MODKEY|Mod4Mask|ShiftMask,    XK_l,      incrogaps,      {.i = -1 } },
+//	{ MODKEY|Mod4Mask|ControlMask,  XK_h,      incrigaps,      {.i = +1 } },
+//	{ MODKEY|Mod4Mask|ControlMask,  XK_l,      incrigaps,      {.i = -1 } },
+//	{ MODKEY|Mod4Mask,              XK_0,      togglegaps,     {0} },
+//	{ MODKEY|Mod4Mask|ShiftMask,    XK_0,      defaultgaps,    {0} },
+//	{ MODKEY,                       XK_y,      incrihgaps,     {.i = +1 } },
+//	{ MODKEY,                       XK_o,      incrihgaps,     {.i = -1 } },
+//	{ MODKEY|ControlMask,           XK_y,      incrivgaps,     {.i = +1 } },
+//	{ MODKEY|ControlMask,           XK_o,      incrivgaps,     {.i = -1 } },
+//	{ MODKEY|Mod4Mask,              XK_y,      incrohgaps,     {.i = +1 } },
+//	{ MODKEY|Mod4Mask,              XK_o,      incrohgaps,     {.i = -1 } },
+//	{ MODKEY|ShiftMask,             XK_y,      incrovgaps,     {.i = +1 } },
+//	{ MODKEY|ShiftMask,             XK_o,      incrovgaps,     {.i = -1 } },
 	{ MODKEY,                       XK_Return, zoom,           {0} },
 	{ MODKEY,                       XK_Tab,    view,           {0} },
 	{ MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
