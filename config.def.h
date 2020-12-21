3d5b974775a7c33ed6a2b03db9f0f4685e785c21
Serhan Ekmekçi <serhan@ekmekci.me>
2020-12-21 15:12:44 +0300

    Revert "system tray (not fully compatible for now)"
    This reverts commit f4cc43f216790b848dd7a9d57201f31a929a09fa.

--- config.def.h
+++ config.def.h
@@ -13,10 +13,6 @@ static const int smartgaps          = 0;        /* 1 means no outer gap when the
 static int showbar            = 1;        /* 0 means no bar */
 static int topbar             = 1;        /* 0 means bottom bar */
 static const int user_bh            = 30;        /* 0 means that dwm will calculate bar height, >= 1 means dwm will user_bh as bar height */
-static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
-static const unsigned int systrayspacing = 0;   /* systray spacing */
-static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
-static const int showsystray        = 1;     /* 0 means no systray */
 static int bb = 2; /* bar border */
 static const unsigned int baralpha = 0x64;
 static const unsigned int borderalpha = 0x64;
