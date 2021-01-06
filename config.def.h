e8317b6c52e1bbfab64037669c7cd93ab4be5e88
Serhan Ekmekçi <serhan@ekmekci.me>
2021-01-06 15:44:28 +0300

    systrayspacing to 0

--- config.def.h
+++ config.def.h
@@ -20,7 +20,7 @@ static const int smartgaps					= 0;    /* 1 means no outer gap when there is onl
 
 /* systray */
 static const unsigned int systraypinning	= 0;	/* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
-static const unsigned int systrayspacing	= 2;	/* systray spacing */
+static const unsigned int systrayspacing	= 0;	/* systray spacing */
 static const int systraypinningfailfirst	= 1;	/* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
 static const int showsystray				= 1;	/* 0 means no systray */
 
