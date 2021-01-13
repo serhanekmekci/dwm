4025545e68f0af76dd15caa3fa26d6978b57311e
Serhan Ekmekçi <serhan@ekmekci.me>
2021-01-14 00:26:29 +0300

    gaps changed, layoutmenu, bar heigh changed and resizehints to 0

--- config.def.h
+++ config.def.h
@@ -6,16 +6,16 @@ static unsigned int snap					= 20;   /* snap pixel */
 /* bar */
 static int showbar							= 1;	/* 0 means no bar */
 static int topbar							= 1;	/* 0 means bottom bar */
-static const int user_bh					= 30;   /* 0 means that dwm will calculate bar height, >= 1 means dwm will user_bh as bar height */
-static const int vertpad					= 20;   /* vertical padding of bar */
+static const int user_bh					= 25;   /* 0 means that dwm will calculate bar height, >= 1 means dwm will user_bh as bar height */
+static const int vertpad					= 10;   /* vertical padding of bar */
 static const int sidepad					= 25;   /* horizontal padding of bar */
 static int bb								= 2;	/* bar border */
 
 /* gaps */
-static const unsigned int gappih			= 20;   /* horiz inner gap between windows */
-static const unsigned int gappiv			= 20;   /* vert inner gap between windows */
+static const unsigned int gappih			= 15;   /* horiz inner gap between windows */
+static const unsigned int gappiv			= 15;   /* vert inner gap between windows */
 static const unsigned int gappoh			= 15;   /* horiz outer gap between windows and screen edge */
-static const unsigned int gappov			= 20;   /* vert outer gap between windows and screen edge */
+static const unsigned int gappov			= 15;   /* vert outer gap between windows and screen edge */
 static const int smartgaps					= 0;    /* 1 means no outer gap when there is only one window */
 
 /* systray */
@@ -91,9 +91,11 @@ static const Rule rules[] = {
 /* layout(s) */
 static float mfact					= 0.55; /* factor of master area size [0.05..0.95] */
 static int nmaster					= 1;    /* number of clients in master area */
-static int resizehints				= 1;    /* 1 means respect size hints in tiled resizals */
 static const int attachdirection	= 3;    /* 0 default, 1 above, 2 aside, 3 below, 4 bottom, 5 top */
+static int resizehints				= 0;    /* 1 means respect size hints in tiled resizals */
 static const int decorhints			= 1;    /* 1 means respect decoration hints */
+
+static const char *layoutmenu_cmd = "dwm_setlayoutmenu";
 
 static const Layout layouts[] = {
 	/* symbol     arrange function */
@@ -221,7 +223,8 @@ static Key keys[] = {
 static Button buttons[] = {
 	/* click                event mask      button          function        argument */
 	{ ClkLtSymbol,          0,              Button1,        setlayout,      {0} },
-	{ ClkLtSymbol,          0,              Button3,        setlayout,      {.v = &layouts[2]} },
+	{ ClkLtSymbol,          0,              Button3,        setlayoutmenu,  {0} },
+	//{ ClkLtSymbol,          0,              Button3,        setlayout,      {.v = &layouts[1]} },
 	{ ClkStatusText,        0,              Button1,        sigdwmblocks,   {.i = 1} },
 	{ ClkStatusText,        0,              Button2,        sigdwmblocks,   {.i = 2} },
 	{ ClkStatusText,        0,              Button3,        sigdwmblocks,   {.i = 3} },
